a3841f096a33985dff6f57d850deb0816bbb215e
lewisc402 <lewisc402@gmail.com>
2016-10-09 15:25:08 +0800

    Updates

---
+++ app.06735deb79123aac3e46.js
@@ -0,0 +1,28 @@
+webpackJsonp([0,3],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var component = __webpack_require__(1);
+	// require('./main.css');
+	__webpack_require__(2);
+
+	document.body.appendChild(component());
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function () {
+	  var element = document.createElement('h1');
+
+	  element.innerHTML = 'Ho worlxxx';
+
+	  return element;
+	};
+
+/***/ }
+]);
